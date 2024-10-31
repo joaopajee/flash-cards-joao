3290a0738e4b79d439066c000af712ddcf992d57
joaopajee <162633168+joaopajee@users.noreply.github.com>
2024-10-31 14:04:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'É uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função',
+    'Um bloco de código que executa uma tarefa'
+)
+
+criaCartao(
+    'Inglês',
+    'Como diz "Oi" em inglês?',
+    'Hi ou Hello'
+)
