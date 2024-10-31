51c3ee3ba68892777a5715f1c38cc8e706ec4f9b
joaopajee <162633168+joaopajee@users.noreply.github.com>
2024-10-31 14:49:41 -0300

    31/10

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 function criaCartao(categoria, pergunta, respost){
+let container = document.getElementById('container');
+let cartao = document.createElement('article');
+cartao.className = 'cartao';
 
-    
+cartao.innerHTML`  
+                <div class="cartao__conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>${perguta}</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>${resposta}</p>
+                    </div>
+                </div>
+                `
+container.appendChild(cartao);
+
+
 }
